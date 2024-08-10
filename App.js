2d8cc2d0cc90caaec9f0059e0e98bf6798fadc8c
akhila-nori <akhila.nori@cumminscollege.in>
2024-08-10 17:20:46 +0530

    Nested DOM in react

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-const heading = React.createElement(
-  "h1",
-  { id: "akhila", xyz: "abcdhfdeog" }, //we can give this attributes to our tags (here h1 tag)
-  "This is heading inside h1"
+// {
+/* <div id="parent">
+  <div>
+    <h1>Hey, I'm h1 tag</h1>
+  </div>
+</div> */
+// }
+
+// code this up and create a REACT DOM for the above
+
+
+
+//here the 3rd argument is the children that you have to pass in
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hey, I'm h1 tag")
+  )
 );
-console.log(heading); //heading h1 is an object ... when you print it, it is an object .... not h1 tag
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); //here the browser will convert the h1 element (javascript object) and put it up as an h1 tag inside the root
+root.render(parent);

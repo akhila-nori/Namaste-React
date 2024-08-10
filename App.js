a320edaf9f32004d635536f8a4251b98b933eb99
akhila-nori <akhila.nori@cumminscollege.in>
2024-08-10 17:08:36 +0530

    fundamentals comments added inside Apps.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const heading = React.createElement(
   { id: "akhila", xyz: "abcdhfdeog" }, //we can give this attributes to our tags (here h1 tag)
   "This is heading inside h1"
 );
+console.log(heading); //heading h1 is an object ... when you print it, it is an object .... not h1 tag
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); //here the browser will convert the h1 element (javascript object) and put it up as an h1 tag inside the root

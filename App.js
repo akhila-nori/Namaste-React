8edd11fda49cb3460469af05f37485d4a3f5229e
akhila-nori <akhila.nori@cumminscollege.in>
2024-08-12 18:08:51 +0530

    fixed merge conflicts

--- App.js
+++ App.js
@@ -25,32 +25,7 @@ const parent = React.createElement(
 
 //JSX
 console.log(parent);
-// {
-/* <div id="parent">
-     <div id="child">
-        <h1>Hey, I'm h1 tag sibling of h2 tag and child of div id child </h1>
-        <h2>Hey, I'm h2 tag sibling of h1 tag and child of div id child </h2>
-     </div>
-   </div> */
-// }
-
-// code this up and create a REACT DOM for the above - sibling - convert into array of different children
-//and wrap the siblings inside the square brackets 
 
 
 
-//here the 3rd argument is the children that you have to pass in
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hey, I'm h1 tag"),
-    React.createElement("h2", {}, "Hey, I'm h2 tag"),
-  ])
-);
 
-//JSX 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);

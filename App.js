14bf5a8804014525cd4f90083ec495d5ebc4babd
Akhila Nori <80121020+akhila-nori@users.noreply.github.com>
2024-08-21 20:13:35 +0530

    updated css file

--- App.js
+++ App.js
@@ -1,29 +1,67 @@
-// {
-/* <div id="parent">
-     <div id="child">
-        <h1>Hey, I'm h1 tag sibling of h2 tag and child of div id child </h1>
-        <h2>Hey, I'm h2 tag sibling of h1 tag and child of div id child </h2>
-     </div>
-   </div> */
-// }
+import React from "react";
+import ReactDOM from "react-dom";
 
-// code this up and create a REACT DOM for the above - sibling - convert into array of different children
-//and wrap the siblings inside the square brackets 
+const HeaderComponent = () => {
+  return (
+    <div className="headerCSS">
+      <div className="logo-conatiner">
+        <img
+          className="logo324grht"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/25539c29532269.55f7d6a0a8c71.jpg"
+        />
+      </div>
 
+      <div className="nav-items1">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
+const RestrauntCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        alt="res-logo"
+        src="https://cookingfromheart.com/wp-content/uploads/2017/09/Veg-Biryani-in-Pressure-Cooker-6.jpg"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+const BodyComponent = () => {
+  return (
+    <div className="bodyCss">
+      <div className="searchCss">Search</div>
+      <div className="res-conatiner">
+        <RestrauntCard />
+      </div>
+    </div>
+  );
+};
 
-//here the 3rd argument is the children that you have to pass in
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hey, I'm h1 tag"),
-    React.createElement("h2", {}, "Hey, I'm h2 tag"),
-  ])
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <BodyComponent />
+    </div>
+  );
+};
 
-//JSX 
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<AppLayout />);

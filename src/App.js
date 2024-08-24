3adfc9f120375a9a4c134dacc2414fa017debb4c
akhila-nori <akhila.nori@cumminscollege.in>
2024-08-24 11:56:59 +0530

    pushing all files inside Namaste-react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const HeaderComponent = () => {
-  return (
-    <div className="headerCSS">
-      <div className="logo-conatiner">
-        <img
-          className="logo324grht"
-          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/25539c29532269.55f7d6a0a8c71.jpg"
-        />
-      </div>
-
-      <div className="nav-items1">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
 const RestrauntCard = (props) => {
   console.log("props is .......", props); //here in props we are only passing resData neeche se
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+const HeaderComponent = () => {
+  return (
+    <div className="headerCSS">
+      <div className="logo-conatiner">
+        <img
+          className="logo324grht"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/25539c29532269.55f7d6a0a8c71.jpg"
+        />
+      </div>
+
+      <div className="nav-items1">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};

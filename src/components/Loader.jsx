9b148537e25ebf0cd35995ea1d91ca34c5768a90
akhila-nori <akhila.nori@cumminscollege.in>
2024-08-27 11:26:02 +0530

    added styling to div conatiner of Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,14 +5,19 @@ const SpinnerButton = () => {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    padding: "100px",
+    height: "100vh", // Full viewport height
+    width: "100vw",
+    backgroundColor: "#f0f0f0",
+  };
+  const spinnerContainerStyle = {
+    dipslay: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   };
   return (
-    <div id="spinner">
-      <RotatingLines
-        strokeWidth="5"
-        strokeColor="gray"
-        style={spinnerStyle}
-      />
+    <div id="spinner" style={spinnerContainerStyle}>
+      <RotatingLines strokeWidth="5" strokeColor="gray" style={spinnerStyle} />
     </div>
   );
 };

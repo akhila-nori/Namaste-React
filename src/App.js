c3dce51276999ebd723955b2214eaa04757b4a78
akhila-nori <akhila.nori@cumminscollege.in>
2024-09-24 08:28:15 +0530

    added route in Apps.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HeaderComponent from "./components/Header";
 import BodyComponent from "./components/Body";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import About from "./components/About";
+import Contact from "./components/ContactUs";
+import Error from "./components/ErrorComponent";
 
 const AppLayout = () => {
   return (
@@ -11,6 +15,21 @@ const AppLayout = () => {
     </div>
   );
 };
-
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+
+root.render(<RouterProvider router={appRouter}/>) //providing router configration to RouterProvider

c476dad4c47c51ed84fc81e7aa446170652239a3
akhila-nori <akhila.nori@cumminscollege.in>
2024-09-29 22:42:27 +0530

    added dynamic routing resID from API useParams()

--- src/components/RestrauntMenu.jsx
+++ src/components/RestrauntMenu.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import ShimmerUI from "./Shimmer";
+import { useParams } from "react-router-dom";
 const RestarauntMenu = () => {
-  return (
+  const [restInfo, setResInfo] = useState(null);
+
+  const p = useParams();
+  console.log('params ....',p)
+
+  //destructuring on the fly resId
+ //   const {resId} = useParams();
+
+
+  //when this page loads make an API call
+  useEffect(() => {
+    fetchMenu();
+  }, []); //useEffect has empty dependecy array, hence it will called once on inital render
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=25396&catalog_qa=undefined&query=Pizza&submitAction=ENTER"
+    );
+    if (!data.ok) {
+      const text = await response.text();
+      console.log("HTTP error: ${data.status}");
+      console.error(`HTTP error! status: ${data.status}, Response: ${text}`);
+    }
+    const json = await data.json();
+    console.log("json data is ......", json);
+    setResInfo(json.data);
+  };
+  return restInfo === null ? (
+    <ShimmerUI />
+  ) : (
     <div className="menu">
-      <h1>Name of the retauraunt</h1>
-      <h2>Menu</h2>
+      <h1>MENU</h1>
       <ul>
         <li>Biryani</li>
         <li>Burgers</li>
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,10 +1,10 @@
-// const ShimmerUI = () => {
-//   return (
-//     <div className="shimmer-container">
-//       <div className="shimmer-cards">Cards</div>
-//       <div className="shimmer-cards">Cards</div>
-//     </div>
-//   );
-// };
+const ShimmerUI = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-cards">Cards</div>
+      <div className="shimmer-cards">Cards</div>
+    </div>
+  );
+};
 
-// export default ShimmerUI;
+export default ShimmerUI;

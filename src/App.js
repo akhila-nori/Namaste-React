20630ca44ad9a1d107827d639143f9e18f509bcc
akhila-nori <akhila.nori@cumminscollege.in>
2024-09-29 09:43:40 +0530

    children route added and used Outlet for rendering of child paths by keeping the Header Component fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HeaderComponent from "./components/Header";
 import BodyComponent from "./components/Body";
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom"
 import About from "./components/About";
 import Contact from "./components/ContactUs";
 import Error from "./components/ErrorComponent";
@@ -11,24 +11,39 @@ const AppLayout = () => {
   return (
     <div className="app">
       <HeaderComponent />
-      <BodyComponent />
+      {/* if path = / then we should have BodyComponent, if pth is /about then we
+      should have About component.
+
+      I want to push these children /about, /contact over here conditionally as per the routes and accordingly that particular componennt 
+      will be rendered on UI. */}
+      <Outlet />
+      {/* Outlet would be filled with children as per the current change in the path ==> and it will be rendered as per the current path */}
     </div>
   );
 };
 const appRouter = createBrowserRouter([
   {
+    //App layout has 3 children and these should be loaded conditonally on UI (above : i.e., where components are rendered) as per the path
     path: "/",
     element: <AppLayout />,
+    children:[
+      {
+        path: "/" ,
+       element: <BodyComponent/>
+
+      },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+    ],
     errorElement: <Error />
   },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "/contact",
-    element: <Contact />
-  }
+  
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
